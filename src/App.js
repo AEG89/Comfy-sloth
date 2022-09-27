033c609407d45be4e1d2a5bbd1a54c45a837fd30
AEG89 <adheeshagunatilake@gmail.com>
2022-09-27 11:54:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// eslint-disable-next-line
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Footer,Navbar,Sidebar } from './components';
+
+
+
+
+import {Home, Products, SingleProduct, 
+  About, Cart, Error, Checkout, PrivateRoute, AuthWrapper} from './Pages'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AuthWrapper>
+    <div>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+        <Route
+            path='checkout'
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+      </Routes>
+      <Footer/>
     </div>
+    </AuthWrapper>
+   
+    
   );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ProductsProvider } from './context/products-context';
+import { FilterProvider} from './context/filter_context'
+import { CartProvider } from './context/cart_context';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { UserProvider } from './context/user_context';
+//name :React Comfy sloth
+//domain :dev-9drl6ij6.us.auth0.com
+//Client Id:gT9uFRqSrvX1aC0jid4FjqStXFGO5WIz
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+    >
+      <UserProvider>
+        <BrowserRouter>
+          <ProductsProvider>
+            <FilterProvider>
+              <CartProvider>
+                
+                  <App />
+              
+              </CartProvider>
+            </FilterProvider>
+          </ProductsProvider>
+        </BrowserRouter>
+      </UserProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
